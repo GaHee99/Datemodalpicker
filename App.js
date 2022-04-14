9a20b47acea33b24454e39447711dc45f8b6c4d7
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2022-04-14 20:47:43 +0900

    datepicker완성

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { SafeAreaView, View, Text, Button } from 'react-native'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
 
-export default function App() {
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [datePickerVisible, setDatePickerVisible] = useState(false)
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true)
+  }
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false)
+  }
+
+  const handleConfirm = (date) => {
+    setSelectedDate(date)
+    hideDatePicker()
+  }
+  function printDate() {
+    const today = selectedDate // 현재 날짜
+
+    const month = today.toLocaleDateString('en-US', {
+      month: '2-digit',
+    })
+    const day = today.toLocaleDateString('en-US', {
+      day: '2-digit',
+    })
+    const dayName = today.toLocaleDateString('ko-KR', {
+      weekday: 'long',
+    })
+
+    return `${month}.${day} ${dayName}`
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          padding: 20,
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}
+          onPress={showDatePicker}
+        >
+          {selectedDate ? printDate() : 'No date selected'}
+        </Text>
+
+        <DateTimePickerModal
+          date={selectedDate}
+          isVisible={datePickerVisible}
+          mode="date"
+          display="inline"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
